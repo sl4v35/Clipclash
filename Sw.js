4425bd4c27c764b377b7cb9ff786412382136eaf
sl4v35 <codyfam0516@gmail.com>
2025-11-23 19:50:44 -0500

    Add service worker for caching assets

---
+++ Sw.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = 'clipcash-v1';
+const ASSETS = [
+    './',
+    './index.html',
+    './manifest.json'
+];
+
+// Install Service Worker
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS);
+        })
+    );
+});
+
+// Activate & Cleanup Old Caches
+self.addEventListener('activate', (e) => {
+    e.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(
+                keys.map((key) => {
+                    if (key !== CACHE_NAME) return caches.delete(key);
+                })
+            );
+        })
+    );
+});
+
+// Fetch Strategy: Stale-While-Revalidate
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then((cachedResponse) => {
+            const fetchPromise = fetch(e.request).then((networkResponse) => {
+                caches.open(CACHE_NAME).then((cache) => {
+                    cache.put(e.request, networkResponse.clone());
+                });
+                return networkResponse;
+            });
+            return cachedResponse || fetchPromise;
+        })
+    );
+});
